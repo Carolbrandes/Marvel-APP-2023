6d4f63950c9c61da365b2c8c08c377144953f7e3
carol <carolbrandes23@gmail.com>
2023-06-23 22:46:15 -0300

    routes configuration and path mapping

---
+++ src/routes/index.tsx
@@ -0,0 +1,19 @@
+import { Suspense, lazy } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+const Home = lazy(() => import('@pages/home'))
+const Hero = lazy(() => import('@pages/hero'))
+
+
+const AppRoutes = () => (
+  <BrowserRouter>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route  path="/" element={<Home />} />
+        <Route  path="/:name" element={<Hero />} />
+      </Routes>
+    </Suspense>
+  </BrowserRouter>
+);
+
+export default AppRoutes
